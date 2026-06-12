f6d58ae5d3970f330880f15428d86c31e65ffe19
Ryan Stewart <yo@ryanforprez.org>
2026-06-12 01:20:10 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const root = __dirname;
+const mime = {
+  '.html': 'text/html', '.css': 'text/css', '.js': 'application/javascript',
+  '.jpg': 'image/jpeg', '.jpeg': 'image/jpeg', '.png': 'image/png',
+  '.gif': 'image/gif', '.mp4': 'video/mp4', '.webm': 'video/webm',
+  '.otf': 'font/otf', '.woff': 'font/woff', '.woff2': 'font/woff2'
+};
+http.createServer((req, res) => {
+  const p = path.join(root, req.url === '/' ? 'index.html' : req.url);
+  fs.readFile(p, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, { 'Content-Type': mime[path.extname(p)] || 'application/octet-stream' });
+    res.end(data);
+  });
+}).listen(3456, () => console.log('Server running on http://localhost:3456'));
